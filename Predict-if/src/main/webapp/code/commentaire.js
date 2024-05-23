c7005c24eeb358599c061cf6962142db89c89a70
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-05-23 15:47:32 +0200

    commentaires html, css, js maj

---
+++ Predict-if/src/main/webapp/code/commentaire.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  // Quand on clique sur le bouton "Terminer la consultation"
+  $("#btn-terminer-consultation").on("click", function () {
+    var commentaire = $("#comentaire-area").val();
+    $.ajax({
+      url: "../ActionServlet",
+      method: "POST",
+      data: {
+        todo: "terminerConsultation",
+        commentaire: commentaire,
+      },
+    })
+      .done(function (response) {
+        if (response.consultationTerminee) {
+          window.location.href = "../html/menuEmploye.html";
+        } else {
+          alert("Erreur lors de la terminaison de la consultation");
+        }
+      })
+      .fail(function (error) {
+        // Fonction appelée en cas d'erreur lors de l'appel AJAX
+        alert("Erreur lors de l'appel AJAX");
+      });
+  });
+});
