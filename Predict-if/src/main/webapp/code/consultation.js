49a5a6466e73b9965bf84f5d7e9bde187d045c59
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-05-25 10:29:21 +0200

    ajout du css statistiques

--- Predict-if/src/main/webapp/code/consultation.js
+++ Predict-if/src/main/webapp/code/consultation.js
@@ -1,36 +1,35 @@
 $(document).ready(function () {
- $.ajax({
+  $.ajax({
     url: "../ActionServlet",
     method: "POST",
     data: {
-     todo: "consultationEnCours",
+      todo: "consultationEnCours",
     },
     dataType: "json",
+  })
+    .done(function (response) {
+      $("#denomination-medium").val(response.medium.denomination);
+      $("#genre-medium").val(response.medium.genre);
+      $("#support-medium").val(response.medium.support);
+      $("#signe-zodiac").val(response.client.profilAstral.signeZodiaque);
+      $("#signe-chinois").val(response.client.profilAstral.signeChinois);
+      $("#animal-totem").val(response.client.profilAstral.animalTotem);
+      $("#couleur").val(response.client.profilAstral.couleur);
+      $("#nom-prenom-client").val(response.client.nomPrenom);
     })
-      .done(function (response) {
-          $("#denomination-medium").val(response.medium.denomination);
-          $("#genre-medium").val(response.medium.genre);
-          $("#support-medium").val(response.medium.support);
-          $("#signe-zodiac").val(response.client.profilAstral.signeZodiaque);
-          $("#signe-chinois").val(response.client.profilAstral.signeChinois);
-          $("#animal-totem").val(response.client.profilAstral.animalTotem);
-          $("#couleur").val(response.client.profilAstral.couleur);
-          $("#nom-prenom-client").val(response.client.nomPrenom);
-       })
-      .fail(function (error) {
-        // Fonction appelée en cas d'erreur lors de l'appel AJAX
-        alert("Erreur lors de l'appel AJAX");
-      });
-  
-      
+    .fail(function (error) {
+      // Fonction appelée en cas d'erreur lors de l'appel AJAX
+      alert("Erreur lors de l'appel AJAX");
+    });
+
   // Quand on clique sur le bouton "Terminer la consultation"
   $("#btn-achever-consult").on("click", function () {
-      $.ajax({
+    $.ajax({
       url: "../ActionServlet",
       method: "GET",
       data: {
         todo: "terminerConsultation",
-      }
+      },
     })
       .done(function (response) {
         window.location.href = "./commentaire.html";
@@ -40,25 +39,25 @@ $(document).ready(function () {
         alert("Erreur lors de l'appel AJAX");
       });
   });
-  
-    // Quand on clique sur le bouton "Generer prediction"
+
+  // Quand on clique sur le bouton "Generer prediction"
   $("#generer-predictions").on("click", function () {
-      var amour = $("#amour-input").val()
-      var sante = $("#sante-input").val()
-      var travail = $("#travail-input").val()
-      $.ajax({
+    var amour = $("#amour-input").val();
+    var sante = $("#sante-input").val();
+    var travail = $("#travail-input").val();
+    $.ajax({
       url: "../ActionServlet",
       method: "GET",
       data: {
         todo: "genererPrediction",
         amour: amour,
         sante: sante,
-        travail: travail
+        travail: travail,
       },
       dataType: "json",
     })
       .done(function (response) {
-          console.log(response);
+        console.log(response);
         $("#prediction-amour").text(response.amour);
         $("#prediction-sante").text(response.sante);
         $("#prediction-travail").text(response.travail);
